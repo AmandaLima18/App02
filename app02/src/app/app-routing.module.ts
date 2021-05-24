f7d816519e0dd107c2dc1e005f3373e9ac79b42e
Nathaliaaguiar <59691090+Nathaliaaguiar@users.noreply.github.com>
2021-05-24 15:32:30 -0300

    ajustes finais

--- app02/src/app/app-routing.module.ts
+++ app02/src/app/app-routing.module.ts
@@ -4,54 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home/title/asc',
+    redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'home/:sort/:order',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'new',
-    loadChildren: () => import('./pages/new/new.module').then( m => m.NewPageModule)
-  },
-  {
-    path: 'contacts',
-    loadChildren: () => import('./pages/contacts/contacts.module').then( m => m.ContactsPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'delete/:id',
-    loadChildren: () => import('./pages/delete/delete.module').then( m => m.DeletePageModule)
-  },
-  {
-    path: 'edit/:id',
-    loadChildren: () => import('./pages/edit/edit.module').then( m => m.EditPageModule)
-  },
-  {
-    path: 'dev/home',
-    loadChildren: () => import('./dev/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'dev/gps',
-    loadChildren: () => import('./dev/gps/gps.module').then( m => m.GpsPageModule)
-  },
-  {
-    path: 'aboutview/:id',
-    loadChildren: () => import('./pages/aboutview/aboutview.module').then( m => m.AboutviewPageModule)
-  },
-
-  // SEMPRE A ÚLTIMA ROTA
-  {
-    path: '**',
-    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
   }
 ];
 
